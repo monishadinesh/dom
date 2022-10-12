52f6c886a3bca9f405b2a7e1bf21a8957eb3209b
monishadinesh <111494894+monishadinesh@users.noreply.github.com>
2022-10-12 12:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo(){
+    var res=document.getElementById("first").value;
+    var res1=document.getElementById("middle").value;
+    var res2=document.getElementById("last").value;
+
+    var res3=document.getElementById("email").value;
+    var res4=document.getElementById("pass").value;
+   
+    console.log(res,res1,res2,res3,res4)
+}
